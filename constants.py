12f4554e053b26237a6fa7057145875b57b8d20c
Clark Pohl <clarklettuce@gmail.com>
2025-12-23 19:14:40 -0500

    factor out constants

---
+++ constants.py
@@ -0,0 +1,10 @@
+ROOT_IP = "198.41.0.4"
+
+# type and class encodings from RFC 1035
+TYPE_A = 1
+TYPE_AAAA = 28
+TYPE_NS = 2
+TYPE_CNAME = 5
+TYPE_TXT = 16
+
+CLASS_IN = 1
--- dns.py
+++ dns.py
@@ -6,18 +6,9 @@ import random
 import socket
 import struct # used for converting python objects into packed (no padding) byte strings, like C structs
 
+from constants import *
+
 random.seed(1)
-
-ROOT_IP = "198.41.0.4"
-
-# type and class encodings from RFC 1035
-TYPE_A = 1
-TYPE_AAAA = 28
-TYPE_NS = 2
-TYPE_CNAME = 5
-TYPE_TXT = 16
-
-CLASS_IN = 1
 
 # dataclass: short for taking all attrs as args and setting them all in init
 @dataclass
